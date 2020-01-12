d6efbd7fad3912a65663c627423be2760ba5edf2
Mohamed Yousif <mmbusif@gmail.com>
2020-01-12 21:23:25 +0200

    Adding test cases

---
+++ raterpc/server/helpers_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func Test_getUSD(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		args  []string
+		want bool
+	}{
+		{"test dollar only", []string{"دولار امريكي", "الدولار"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := getUSD(tt.args)
+			if got != tt.want {
+				t.Errorf("getUSD() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
