b06393a2e289c92838a6a805712c208be2ec6d9e
Mohamed Yousif <mmbusif@gmail.com>
2020-03-14 19:13:57 +0200

    adding more tests to parser

--- raterpc/server/helpers_test.go
+++ raterpc/server/helpers_test.go
@@ -5,11 +5,11 @@ import "testing"
 func Test_getUSD(t *testing.T) {
 
 	tests := []struct {
-		name  string
-		args  []string
+		name string
+		args []string
 		want bool
 	}{
-		{"test dollar only", []string{"دولار امريكي", "الدولار"}, true},
+		{"test dollar only", []string{"دولار امريكي", "الدولار", "دولار&nbsp;الامريكي<"}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
